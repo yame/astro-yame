6761b54768b7b8a83bfcfc4e206c8311284a85c9
yame <zhengym912@qq.com>
2025-07-01 10:06:37 +0800

    添加Preact集成，更新Astro配置以支持JSX语法，并在tsconfig中设置jsxImportSource为Preact；在首页引入Greeting组件以显示问候信息。

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://hiyame.netlify.app/"
+  site: "https://hiyame.netlify.app/",
+  integrations: [preact()]
 });
